deadfdcb932090a3b8221293a77130b747b0c39f
Patrik <p.seba@icloud.com>
2024-02-27 12:38:01 +0100

    remove useless stuff

--- src/mikro-config.ts
+++ src/mikro-config.ts
@@ -4,7 +4,7 @@ import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 export const config = defineConfig<PostgreSqlDriver>({
   driver: PostgreSqlDriver,
-  entities: ['./src/entities'], // path to our TS entities (src), relative to `baseDir`
+  entities: ['./src/entities'],
   dbName: 'mikro_dev_1',
   host: 'localhost',
   port: 5432,
@@ -12,16 +12,15 @@ export const config = defineConfig<PostgreSqlDriver>({
   password: 'test',
   migrations: {
     tableName: 'migrations',
-    // path: `./src/migrations`,
     pathTs: `./src/migrations`,
     glob: '+([A-Za-z0-9]|-)+([0-9]).{ts,js}',
     transactional: true,
-    disableForeignKeys: false, // Disabled until its needed, because it requires superuser
-    allOrNothing: true, // wrap all migrations in master transaction
-    dropTables: true, // allow or disable table dropping
-    safe: false, // allow or disable table and column dropping
-    emit: 'ts', // migration generation mode
-    snapshotName: '.snapshot-datapacket',
+    disableForeignKeys: false,
+    allOrNothing: true,
+    dropTables: true,
+    safe: false,
+    emit: 'ts',
+    snapshotName: '.snapshot',
   },
   extensions: [Migrator],
-})
+});
